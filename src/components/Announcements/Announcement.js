5055206eaf487c3bc8b18d457b33441c4ed91ec7
safemammoth <safemammoth@gmail.com>
2021-04-21 18:34:36 +0800

    Added Airdrop link to Airdrop announcement

--- src/components/Announcements/Announcement.js
+++ src/components/Announcements/Announcement.js
@@ -1,13 +1,16 @@
+import { Link } from 'gatsby';
 import React from 'react';
 import "./announcement.css"
 
-function Announcement({title,content}){
+function Announcement({title,content,linkname,link}){
         
     return(
         <div className="announcement-container">
             <div className="announcement-title">{title}</div>
             <div className="announcement-content">
                     {content}
+                    <br></br>
+            <Link to={link}>{linkname}</Link>
             </div>
         </div>
     )
--- src/components/Announcements/AnnouncementSection.js
+++ src/components/Announcements/AnnouncementSection.js
@@ -7,8 +7,9 @@ return(
     <div className="announcement-section">
     <h2>{title}</h2>
     <div className="announcements">
-    <Announcement title="Airdrop!!! Airdrop!!!" content="We will be giving away 50000 $MMTH each for the first 200 to complete the tasks listed on this link
-        For more information about our SAFE Mammoth, head on to our telegram"/>
+    <Announcement title="Airdrop!!! Airdrop!!!" content="We will be giving away 50000 $MMTH each for the first 200 to complete the tasks listed on the link below.
+        For more information about our SAFE Mammoth, head on to our telegram." linkname="Airdrop" 
+        link="https://docs.google.com/forms/d/e/1FAIpQLSdsuIZ-IFL0J8rdlsW9EqZDlDMy7BPSJ4unwg2rRGWUAVBnyw/viewform"/>
     </div>
     </div>
 )
